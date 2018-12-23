eb80a18034c6e632892d0c9ed5aac6f01d24a707
Mumtaz Khan <makcalif@yahoo.com>
2018-12-23 15:34:15 -0600

    added step verifier with delay using thenConsumeWhile

--- src/test/java/com/cognizant/labs/spring/flux/awesomespringflux/FluxMonoTest.java
+++ src/test/java/com/cognizant/labs/spring/flux/awesomespringflux/FluxMonoTest.java
@@ -3,12 +3,15 @@ package com.cognizant.labs.spring.flux.awesomespringflux;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
 import reactor.util.function.Tuple2;
 
+import java.io.IOException;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 //http://sinhamohit.com/writing/reactor-core-tutorial
 
@@ -49,11 +52,99 @@ public class FluxMonoTest {
         Flux<Long> delay = Flux.interval(Duration.ofSeconds(3));
         Flux<String> firstNamesWithDelay = firstNames.zipWith(delay, (s, l) -> s);
 
+        // log let's you see the delayed execution
+        // while the subsequent iteration is without delay
         List list = Flux.zip(firstNames, firstNamesWithDelay)
                 .log()
                 .collectList()
                 .block();
 
         list.forEach(System.out::println);
+    }
+
+    @Test
+    public void delay2() {
+        Flux<String> firstNames = Flux.fromIterable(Arrays.asList("A", "B", "C"));
+        Flux<Long> delay = Flux.interval(Duration.ofSeconds(3));
+        Flux<String> firstNamesWithDelay = firstNames.zipWith(delay, (s, l) -> s);
+        // todo come back to this later
+    }
+
+    @Test
+    public void range() {
+        List range = Flux.range(1, 5)
+                //.parallel(10)
+                .delayElements(Duration.ofSeconds(3))
+                .log()
+                .collectList()
+                .block();
+
+                range.forEach(System.out::println);
+    }
+
+    @Test
+    public void doOnNext() {
+
+        // todo ... didn't work.. come back to this later
+        Flux f = Flux.range(1, 5)
+                //.parallel(10)
+                .delayElements(Duration.ofSeconds(3))
+                .doOnNext(System.out::println);
+        f.subscribe();
+    }
+
+    @Test
+    public void mergeFluxWithDelayFlux() {
+        Flux<String> strs = Flux.just("A", "B", "C");
+        Flux<Long> delay = Flux.interval(Duration.ofSeconds(3));
+
+        Flux<String> strsWithDelay = strs.zipWith(delay, (str, longDelay) -> str);
+
+        strsWithDelay
+                .collectList()
+                .block()
+                .forEach(System.out::println);
+
+        StepVerifier.create(strsWithDelay)
+                .thenConsumeWhile((e) -> true, System.out::println);
+    }
+
+    @Test
+    public void verifyConsumeWithDelay() {
+        Flux<Long> range = Flux.interval(Duration.ofSeconds(1)).take(3);
+
+        StepVerifier.create(range)
+                .expectNext(0L, 1L, 2L)
+                .expectComplete()
+                .verify();
+
+        StepVerifier.create(range)
+                .thenConsumeWhile((e) -> e.longValue() >= 0, System.out::println)
+                .expectComplete()
+                .verify();
+
+    }
+
+    @Test
+    public void monoException() {
+        Flux foo = Flux.just("foo", "bar").concatWith(Mono.error(new RuntimeException()));
+
+        StepVerifier.create(foo)
+                .expectNext("foo", "bar")
+                .expectError(RuntimeException.class)
+                .verify();
+    }
+
+    @Test
+    public void expectNextMatches() {
+        Flux foo = Flux.just("foo", "bar");
+        Predicate<String> first = (e) -> e.equalsIgnoreCase("foo");
+        Predicate<String> second = (e) -> e.equalsIgnoreCase("bar");
+        StepVerifier.create(foo)
+                .expectNextMatches(first)
+                .consumeNextWith( System.out::println)
+                .expectComplete()
+                .verify();
+
     }
 }
