f2aeb73a4cb312962552656b414143bad1019101
Mumtaz Khan <makcalif@yahoo.com>
2018-12-22 19:51:14 -0600

    unit test with delay (using block and log)

--- src/test/java/com/cognizant/labs/spring/flux/awesomespringflux/FluxMonoTest.java
+++ src/test/java/com/cognizant/labs/spring/flux/awesomespringflux/FluxMonoTest.java
@@ -4,8 +4,11 @@ import org.junit.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+import reactor.util.function.Tuple2;
 
+import java.time.Duration;
 import java.util.Arrays;
+import java.util.List;
 
 //http://sinhamohit.com/writing/reactor-core-tutorial
 
@@ -27,10 +30,32 @@ public class FluxMonoTest {
 
         Flux<String> fluxFromIterable = Flux.fromIterable(Arrays.asList("mike", "john", "hardy"));
         StepVerifier.create(fluxFromIterable).expectNext("mike", "john", "hardy").verifyComplete();
+    }
 
+    @Test
+    public void zip() {
+        Flux<String> firstNames = Flux.fromIterable(Arrays.asList("A", "B", "C"));
+        Flux<String> lastNames = Flux.fromIterable(Arrays.asList("1", "2", "3"));
 
+        Flux<String> zipped = Flux.zip(firstNames, lastNames)
+                .map(a -> a.getT1() + "=" + a.getT2());
 
+        StepVerifier.create(zipped).expectNext("A=1", "B=2", "C=3").verifyComplete();
+    }
 
+    @Test
+    public void delay() {
+        Flux<String> firstNames = Flux.fromIterable(Arrays.asList("A", "B", "C"));
+        Flux<Long> delay = Flux.interval(Duration.ofSeconds(3));
+        //Flux<String> delay = Flux.fromIterable(Arrays.asList("1", "2", "3"));
+        Flux<String> firstNamesWithDelay = firstNames.zipWith(delay, (s, l) -> s);
 
+        //Flux<String> zipped = Flux.zip(firstNames, firstNamesWithDelay)
+        List list = Flux.zip(firstNames, firstNamesWithDelay)
+                .log()
+                .collectList()
+                .block();
+
+        list.forEach(System.out::println);
     }
 }
