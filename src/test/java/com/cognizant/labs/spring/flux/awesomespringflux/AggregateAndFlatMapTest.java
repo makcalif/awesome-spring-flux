e8b94bef2fed648a9b23cf0d91271e7ab7ab4798
Mumtaz Khan <makcalif@yahoo.com>
2019-01-05 16:27:51 -0600

    added random delay using for loop

--- src/test/java/com/cognizant/labs/spring/flux/awesomespringflux/AggregateAndFlatMapTest.java
+++ src/test/java/com/cognizant/labs/spring/flux/awesomespringflux/AggregateAndFlatMapTest.java
@@ -4,13 +4,19 @@ import com.cognizant.labs.spring.flux.awesomespringflux.domain.Employee;
 import com.cognizant.labs.spring.flux.awesomespringflux.domain.Equipment;
 import org.junit.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.MonoSink;
 import reactor.test.StepVerifier;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 public class AggregateAndFlatMapTest {
 
@@ -40,17 +46,25 @@ public class AggregateAndFlatMapTest {
 
     @Test
     public void zipAndPrintComplexFlux() {
-        Flux<Employee> employees = Flux.fromIterable(Arrays.asList(
+
+        Flux<Employee> employeeBasic = Flux.fromIterable(Arrays.asList(
                 new Employee("123", "John", "Doe"),
                 new Employee("234", "Michael", "Phelps"),
                 new Employee("234", "Andre", "Aggasi")
         ));
 
-        Flux<Equipment> equipment = Flux.fromIterable(Arrays.asList(
+        Flux<Long> delay = randomDelayFlux();
+        Flux<Employee> employees = Flux.zip(delay, employeeBasic)
+                .map(e -> e.getT2());
+
+        Flux<Equipment> equipmentBasic = Flux.fromIterable(Arrays.asList(
                 new Equipment(12L, "computer", "hardware"),
                 new Equipment(12L, "laptop", "hardware"),
                 new Equipment(12L, "windows", "software")
         ));
+
+        Flux<Equipment> equipment = Flux.zip(delay, equipmentBasic)
+                .map(e -> e.getT2());
 
         Function<Employee, Flux<String>> mapper = e -> Flux.just(e.toString());
         Flux<String> empStrFlux = employees.flatMap( mapper);
@@ -62,5 +76,50 @@ public class AggregateAndFlatMapTest {
         merged.doOnNext(System.out::println)
                 .blockLast();
 
+    }
+
+    private Flux<Long> randomDelayFlux() {
+        ArrayList<Flux<Long>> fluxList = new ArrayList<>();
+        for(int i=0; i < 5; i++) {
+            fluxList.add(Flux.create((FluxSink<Long> sink) -> {
+                sink.next(getRandomDelayMono().block());
+                sink.complete();
+            }));
+        }
+
+        Flux<Long> longFlux = Flux.create((FluxSink<Long> sink) -> {
+            sink.next(getRandomDelayMono().block());
+            sink.complete();
+        });
+
+        //return longFlux;
+        return Flux.mergeSequential(fluxList);
+    }
+
+    @Test
+    public void testRandom() {
+        randomDelayFlux().doOnNext(System.out::println)
+                .blockLast();
+    }
+
+    /*
+    //private Flux<Long> randomDelayFlux() {
+//        return Flux.create(sink -> {
+//            sink.next(getRandomDelayMono());
+//        });
+
+        Flux<Mono<Long>> created  = Flux.create(sink -> {
+           sink.next(getRandomDelayMono());
+        });
+        return created;
+
+    }*/
+
+
+    private Mono<Long> getRandomDelayMono() {
+        int random = ThreadLocalRandom.current().nextInt(1,10);
+        System.out.println("delay:" + random);
+        Mono<Long> delay = Mono.delay(Duration.ofSeconds(random)).then(Mono.just(1L));
+        return delay;
     }
 }
