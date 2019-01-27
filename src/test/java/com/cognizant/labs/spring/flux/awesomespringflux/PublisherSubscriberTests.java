53ec8a9207889d2fe70ff449714cc95e298bb27c
Mumtaz Khan <makcalif@yahoo.com>
2019-01-27 16:40:15 -0600

    infinite flux test with delay

--- src/test/java/com/cognizant/labs/spring/flux/awesomespringflux/PublisherSubscriberTests.java
+++ src/test/java/com/cognizant/labs/spring/flux/awesomespringflux/PublisherSubscriberTests.java
@@ -4,12 +4,12 @@ package com.cognizant.labs.spring.flux.awesomespringflux;
 import org.junit.Test;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
+import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.TreeMap;
+import java.time.Duration;
+import java.util.*;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -20,6 +20,7 @@ public class PublisherSubscriberTests {
 
         //Flux<Integer> intFlux = Flux.fromIterable(Arrays.asList(1, 2, 3, 4,5));
         Flux<Integer> intFlux = Flux.fromStream(IntStream.range(0,10).boxed());
+        // flux range is also available : Flux.range(1,10)
         intFlux.subscribe(System.out::println);
     }
 
@@ -63,4 +64,46 @@ public class PublisherSubscriberTests {
                 .subscribe(subscriber);
 
     }
+
+    @Test
+    public void testInfiniteFlux() throws Exception {
+        ConnectableFlux<Object> connectableFlux = Flux.create(fluxSink -> {
+            while (true) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                }
+                fluxSink.next(System.currentTimeMillis());
+            }
+        })
+        .publish();
+
+        connectableFlux.log()
+                .subscribe( t ->  {System.out.println("Subscirber 1:" + t);});
+
+        connectableFlux.log()
+                .subscribe( t ->  {System.out.println("Subscirber 2:" + t);});
+
+        connectableFlux.connect();
+
+
+    }
+
+    @Test
+    public void testInfiniteFluxWithDelayedSampling() throws Exception {
+        ConnectableFlux<Object> connectableFlux = Flux.create(fluxSink -> {
+            while (true) {
+                fluxSink.next(System.currentTimeMillis());
+            }
+        })
+        .sample(Duration.ofSeconds(2))
+        .publish();
+
+        connectableFlux.log()
+                .subscribe( t ->  {System.out.println("Subscirber 1:" + t);});
+
+        connectableFlux.connect();
+
+    }
+    // add tests for delay plus step verifier in ppt
 }
